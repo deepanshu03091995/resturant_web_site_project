7b82922494e3965e3cd4be5da7eeac739c29a318
Deepanshu03091995 <43910264+deepanshu03091995@users.noreply.github.com>
2024-01-15 22:26:37 +0530

    Category added

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use("/api/v1/test", require("./routes/testRoute"));
 app.use("/api/v1/auth", require("./routes/authRoutes"));
 app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/resturant/", require("./routes/resturantRoutes"));
+app.use("/api/v1/category", require("./routes/categoryRoutes"));
 app.get("/", (req, res) => {
   return res
     .status(200)
